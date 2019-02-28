9929550e674a13e3a3ebdf30e00a27a2c9396e98
Mason Wilie <masonwilie@gmail.com>
2019-02-28 11:11:23 -0700

    BoardCell and IntBoard classes added

---
+++ src/experiment/BoardCell.java
@@ -0,0 +1,13 @@
+package experiment;
+
+/* Represents one cell in the game board grid.
+ * 
+ */
+public class BoardCell {
+	private int row, column;
+	
+	
+	
+	
+	
+}
---
+++ src/experiment/IntBoard.java
@@ -0,0 +1,33 @@
+package experiment;
+
+import java.util.Map;
+import java.util.Set;
+
+/* Contains the grid and adjacency lists
+ * 
+ */
+public class IntBoard {
+	
+	private BoardCell[][] grid;
+	private Set<BoardCell> targets;
+	private Map<BoardCell, Set<BoardCell>> adjacencies;
+	
+	public IntBoard() {
+		
+	}
+	
+	private void calcAdjacencies() {
+		
+	}
+	
+	public Set<BoardCell> getAdjList(BoardCell cell){
+		return adjacencies[cell];
+	}
+	
+	public void calcTargets(BoardCell startCell, int pathLength) {
+		
+	}
+	
+	
+	
+}
