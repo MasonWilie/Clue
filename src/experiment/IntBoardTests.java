132e722cf10d346c0dc4826adf29119204392ccf
Hunter Rich <hrich@mymail.mines.edu>
2019-02-28 11:19:20 -0700

    Started IntBoardTests class

---
+++ src/experiment/IntBoardTests.java
@@ -0,0 +1,75 @@
+package experiment;
+
+public class IntBoardTests {
+	@Before
+    public void beforeAll() {
+       board = new IntBoard();  // constructor should call calcAdjacencies() so you can test them
+    }
+
+	/*
+	 * Test adjacencies for top left corner
+	 */
+	@Test
+	public void testAdjacency00()
+	{
+		BoardCell cell = board.getCell(0,0);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertEquals(2, testList.size());
+	}
+	
+	@Test
+	public void testAdjacency33()
+	{
+		BoardCell cell = board.getCell(3,3);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(4, 3)));
+		assertTrue(testList.contains(board.getCell(3, 4)));
+		assertTrue(testList.contains(board.getCell(2, 3)));
+		assertTrue(testList.contains(board.getCell(3, 2)));
+		assertEquals(2, testList.size());
+	}
+	
+	@Test
+	public void testAdjacency13()
+	{
+		BoardCell cell = board.getCell(1,3);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1, 4)));
+		assertTrue(testList.contains(board.getCell(2, 3)));
+		assertTrue(testList.contains(board.getCell(0, 3)));
+		assertTrue(testList.contains(board.getCell(0, 4)));
+		assertEquals(2, testList.size());
+	}
+	
+	@Test
+	public void testAdjacency30()
+	{
+		BoardCell cell = board.getCell(3,0);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertEquals(2, testList.size());
+	}
+	
+	@Test
+	public void testAdjacency11()
+	{
+		BoardCell cell = board.getCell(1,1);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertEquals(2, testList.size());
+	}
+	
+	@Test
+	public void testAdjacency22()
+	{
+		BoardCell cell = board.getCell(2,2);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertEquals(2, testList.size());
+	}
+}
