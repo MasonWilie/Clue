21d7304f7a455dd5c1ff3af4939cbc45a32a6a5d
Mason Wilie <masonwilie@gmail.com>
2019-03-05 11:22:47 -0700

    Board class and DoorDirection enum type added

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,21 @@
+package clueGame;
+
+/**
+ * BoardCell Class:
+ * Represents one cell of the game board
+ * 
+ * @author Hunter Rich
+ * @author Mason Wilie
+ * 
+ */
+public class BoardCell {
+	private int row, column;
+	
+	public BoardCell(int setRow, int setColumn) {
+		row = setRow;
+		column = setColumn;
+	}
+	
+	
+	
+}
