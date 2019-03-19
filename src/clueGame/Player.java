08c08362d44400c8f31c0142a5dc9fcbc78e2278
Mason Wilie <masonwilie@gmail.com>
2019-03-19 11:30:24 -0600

    Shell of Player Class Created
    Added playerName, row, column, and color member variables. Added
    disproveSuggestion public member function.

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -1,5 +1,15 @@
 package clueGame;
 
-public class Player {
+import com.sun.prism.paint.Color;
 
+public class Player {
+	String playerName;
+	int row;
+	int column;
+	Color color;
+	
+	public Card disproveSuggestion(Solution suggestion) {
+		return null;
+	}
+	
 }
