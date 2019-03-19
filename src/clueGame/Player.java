0a899a7c970ca8649865e24e51a905d0054077d8
Mason Wilie <masonwilie@gmail.com>
2019-03-19 11:43:36 -0600

    Getters and setters created

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -8,8 +8,28 @@ public class Player {
 	private int column;
 	private Color color;
 	
+
 	public Card disproveSuggestion(Solution suggestion) {
 		return null;
 	}
+	
+	// For tests only
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+	
 	
 }
