544c5a32549f9e621a590892087a170f73274688
Mason Wilie <masonwilie@gmail.com>
2019-04-02 11:18:40 -0600

    Created file gameActionTests.java

---
+++ src/tests/gameActionTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+
+
+/**
+ * gameActionTests Class:
+ * Tests for selecting a target location, checking an accusation, disproving a suggestion
+ * handling a suggestion, and creating a suggestion.
+ * 
+ * @author Hunter Rich
+ * @author Mason Wilie
+ * 
+ */
+
+public class gameActionTests {
+
+}
