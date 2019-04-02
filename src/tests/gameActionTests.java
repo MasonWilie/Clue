3ef02d7f55e54bc3f9c0f0d868a55b0e9f0875a5
Hunter Rich <hrich@mymail.mines.edu>
2019-04-02 11:47:48 -0600

    Wrote testTargetSelection() test (not tested yet)

--- src/tests/gameActionTests.java
+++ src/tests/gameActionTests.java
@@ -1,6 +1,15 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
 
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import clueGame.Board;
+import clueGame.ComputerPlayer;
+import clueGame.BoardCell;
 
 /**
  * gameActionTests Class:
@@ -13,5 +22,29 @@ package tests;
  */
 
 public class gameActionTests {
-
+	private static Board board;
+	@Before
+	public void testTargetSetup() {
+		board = Board.getInstance();
+		board.setConfigFiles("Board_Layout.csv", "ClueRooms.txt", "players.txt", "cards.txt");		
+		board.initialize();
+	}
+	
+	@Test
+	public void testTargetSelection() {
+		ComputerPlayer cPlayer = new ComputerPlayer();
+		cPlayer.setRow(5);
+		cPlayer.setRow(6);
+		int currentRow = cPlayer.getRow();
+		int currentCol = cPlayer.getColumn();
+		board.calcTargets(currentRow, currentCol, 3);
+		Set<BoardCell> theTargets = board.getTargets();
+		
+		//run through choosing many different targets and making sure they are in the list of targets
+		for (int i = 0; i < 20; i++) {
+			BoardCell aTarget = cPlayer.chooseLocation();
+			assertTrue(theTargets.contains(aTarget));
+		}
+		
+	}
 }
